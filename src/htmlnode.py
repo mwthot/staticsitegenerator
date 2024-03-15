44620848a15aa3eabcae7ff273ac1150f574f591
mwthot <mw.thot@gmail.com>
2024-03-14 22:20:55 -0700

    Finished htmlnode class and test

---
+++ src/htmlnode.py
@@ -0,0 +1,17 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag 
+        self.value = value 
+        self.children = children 
+        self.props = props
+
+    def to_html(self): 
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self): 
+        if self.props == None: 
+            return ""
+        props_html = ""
+        for prop in self.props: 
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
