269170f208714e19e2023fd5849a01e6c8911208
mwthot <mw.thot@gmail.com>
2024-03-17 18:30:03 -0700

    Add LeafNode and matching unit tests.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self): 
@@ -13,6 +13,14 @@ class TestHTMLNode(unittest.TestCase):
             node.props_to_html(), 
             ' class="greeting" href="boot.dev"',
         )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self): 
+        node = LeafNode(None, "Hello world!")
+        self.assertEqual(node.to_html(), "Hello, wolrd!")
 
 if __name__ == "__main__": 
     unittest.main()
