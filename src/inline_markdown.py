bc916a786949bb02184a0e71801f06706ecd3c44
mwthot <mw.thot@gmail.com>
2024-03-18 22:34:37 -0700

    More inline work.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,31 +1,40 @@
 import re
 
-from textnode import(
+from textnode import (
     TextNode,
     text_type_text,
     text_type_bold,
     text_type_italic,
     text_type_code,
-    text_type_image, 
+    text_type_image,
     text_type_link,
-    
 )
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type): 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    for old_node in old_nodes: 
+    for old_node in old_nodes:
         if old_node.text_type != text_type_text:
-            new_nodes.append(old_nodes)
+            new_nodes.append(old_node)
+            continue
         split_nodes = []
         sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0: 
+        if len(sections) % 2 == 0:
             raise ValueError("Invalid markdown, bold section not closed")
-        for i in range(len(sections)): 
-            if sections[i] == "": 
+        for i in range(len(sections)):
+            if sections[i] == "":
                 continue
-            if i % 2 == 0: 
+            if i % 2 == 0:
                 split_nodes.append(TextNode(sections[i], text_type_text))
-            else: 
+            else:
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
